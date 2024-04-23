4e1168fa60283e55a79db41df66a2020cddcaa86
AnnaHtslk <hannah.htslk@gmail.com>
2024-04-23 12:21:38 +0300

    upd mobile-menu and benefits

--- src/js/mobile-menu/mobile-menu.js
+++ src/js/mobile-menu/mobile-menu.js
@@ -1,14 +1,19 @@
 const mobMenu = document.querySelector('.backdrop');
 const burgerBtn = document.querySelector('.burger-menu');
 const closeBtn = document.querySelector('.mobile-menu-btn')
+const body = document.querySelector('body');
 console.log(burgerBtn);
 
 
 const showHideMenu = () => {
     mobMenu.classList.toggle('is-open');
-    
+    body.classList.toggle('body-is-hidden');
 };
 
 burgerBtn.addEventListener('click', showHideMenu);
 closeBtn.addEventListener('click', showHideMenu);
 
+document.querySelectorAll('.nav-link').forEach(link => link.addEventListener('click', () => { 
+    mobMenu.classList.remove('is-open');
+    body.classList.remove('body-is-hidden');
+}))
