1973aa10af5d29a61385d6fa7e474a7d5de68e8f
AnnaHtslk <hannah.htslk@gmail.com>
2024-04-23 10:50:32 +0300

    Update mobile-menu.js

--- src/js/mobile-menu/mobile-menu.js
+++ src/js/mobile-menu/mobile-menu.js
@@ -1,12 +1,14 @@
 const mobMenu = document.querySelector('.backdrop');
 const burgerBtn = document.querySelector('.burger-menu');
+const closeBtn = document.querySelector('.mobile-menu-btn')
 console.log(burgerBtn);
 
 
 const showHideMenu = () => {
-    mobMenu.classList.toggle('is-open')
+    mobMenu.classList.toggle('is-open');
+    
 };
 
 burgerBtn.addEventListener('click', showHideMenu);
-
+closeBtn.addEventListener('click', showHideMenu);
 
