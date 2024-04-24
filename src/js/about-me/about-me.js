ab69090a6e2cf6307f45efdf50eb7d5545ed9dd5
AnnaHtslk <hannah.htslk@gmail.com>
2024-04-24 11:06:10 +0300

    upd

--- src/js/about-me/about-me.js
+++ src/js/about-me/about-me.js
@@ -1,9 +1,40 @@
 import Swiper from 'swiper';
 
 import { Navigation } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
 
 
+
+const mySwiper = new Swiper(".swiper", {
+    direction: 'horizontal',
+    slidesPerView: 1,
+    spaceBetween: 0,
+    modules: [ Navigation ],
+    breakpoints: {
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 0
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 0
+        },
+        1440: {
+            slidesPerView: 6,
+            spaceBetween: 0
+        }
+    },
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+
+    },
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+    },
+    mousewheel: {
+    invert: true,
+  },
+});
 
    
